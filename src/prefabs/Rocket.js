04bff0b588d76a2972c5fae64e9088cdb1cd4bb0
Lou Yonzon <Marytalens@gmail.com>
2022-04-11 16:44:19 -0700

    asset fix

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -13,6 +13,8 @@ class Rocket extends Phaser.GameObjects.Sprite{
         if(!this.isFiring){
             if(keyLEFT.isDown && this.x >= borderUISize + this.width){
                 this.x-=this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <=game.config.width-borderUISize-this.width){
+                this.x+=this.moveSpeed;
             }
         }
         //fire button
@@ -28,5 +30,9 @@ class Rocket extends Phaser.GameObjects.Sprite{
             this.isFiring=false;
             this.y=game.config.height-borderUISize-borderPadding;
         }
+    }
+    reset(){
+        this.isFiring=false;
+        this.y=game.config.height-borderUISize-borderPadding;
     }
 }
