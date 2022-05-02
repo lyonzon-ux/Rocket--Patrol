5573cfbc86585be845d7562e1cc63b7beffdad33
Lou Yonzon <Marytalens@gmail.com>
2022-05-02 16:24:39 -0700

    edit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,7 +12,7 @@ class Play extends Phaser.Scene {
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
-
+ 
     create(){
         //place tile sprite
         this.starfield=this.add.tileSprite(0,0,640,480,'starfield').setOrigin(0,0);
